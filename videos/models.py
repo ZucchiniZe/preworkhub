0fe5dd5dcae31da628d216f6398dc4384a0b54cb
Alex B <me@alexb.io>
2017-10-05 02:38:18 -0700

    update gitignore, add beginnings of model(s)

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class PreworkVideo(models.Model):
+    CALCULUS = 'calc'
+    STATISTICS = 'stat'
+    CLASS_CHOICES = (
+        (CALCULUS, 'Calculus'),
+        (STATISTICS, 'Statistics'),
+    )
+    subject = models.CharField(max_length=4, choices=CLASS_CHOICES, required=True)
+    title = models.CharField()
