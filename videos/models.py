f78acea633e586bb43c10d1eff6823220e232428
Alex B <me@alexb.io>
2017-10-06 16:43:34 -0700

    add more to data model for videos

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,4 @@
+import re
 from django.db import models
 
 
@@ -8,5 +9,28 @@ class PreworkVideo(models.Model):
         (CALCULUS, 'Calculus'),
         (STATISTICS, 'Statistics'),
     )
-    subject = models.CharField(max_length=4, choices=CLASS_CHOICES, required=True)
-    title = models.CharField()
+
+    title = models.CharField(max_length=100)
+    created = models.DateField(auto_now_add=True)
+    subject = models.CharField(max_length=4, choices=CLASS_CHOICES)
+
+    # These are SmallIntegerFields because we aren't going to be having class numbers above 32767
+    class_num = models.SmallIntegerField()
+    video_num = models.SmallIntegerField()
+
+    slug = models.SlugField()  # TODO: Should be prepopulated with f"{class_num}v{video_num}" f"{class_num}.{video_num}"
+
+    notes = models.TextField()
+    video_link = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def video_id(self):
+        pattern = re.compile('d\/(0[A-Z])\w+\/view')
+        match = pattern.match(self.video_link)
+        if match:
+            return match.lastgroup()
+        else:
+            return ''
