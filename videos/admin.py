ab00469bb8e8fff0ed6530ac8fa0c916fb02ab08
Alex B <me@alexb.io>
2017-10-06 17:25:03 -0700

    final model changes

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PreworkVideo
+
+
+@admin.register(PreworkVideo)
+class PreworkVideoAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('class_num', 'video_num')}
+    fields = ('title', 'video_link', 'subject', ('class_num', 'video_num'), 'slug', 'notes')
+
--- videos/models.py
+++ videos/models.py
@@ -13,24 +13,28 @@ class PreworkVideo(models.Model):
     title = models.CharField(max_length=100)
     created = models.DateField(auto_now_add=True)
     subject = models.CharField(max_length=4, choices=CLASS_CHOICES)
+    video_link = models.CharField(max_length=100)
 
     # These are SmallIntegerFields because we aren't going to be having class numbers above 32767
     class_num = models.SmallIntegerField()
-    video_num = models.SmallIntegerField()
+    video_num = models.SmallIntegerField(default=1)
 
-    slug = models.SlugField()  # TODO: Should be prepopulated with f"{class_num}v{video_num}" f"{class_num}.{video_num}"
+    slug = models.SlugField()
 
-    notes = models.TextField()
-    video_link = models.CharField(max_length=100)
+    notes = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
 
     @property
     def video_id(self):
-        pattern = re.compile('d\/(0[A-Z])\w+\/view')
-        match = pattern.match(self.video_link)
+        pattern = re.compile('(0[A-Z])\w+')
+        match = pattern.search(self.video_link)
         if match:
-            return match.lastgroup()
+            return match.group()
         else:
             return ''
+
+    @property
+    def embed_link(self):
+        return f"https://drive.google.com/file/d/{self.video_id}/preview"
