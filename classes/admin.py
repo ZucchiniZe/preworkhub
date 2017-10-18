2a330b8be8aa9f43429118d7a8dd7e2ffc95c6bd
Alex B <me@alexb.io>
2017-10-18 11:25:24 -0700

    add video inline to class date and update fixture.json

--- classes/admin.py
+++ classes/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
 from .models import Student, Subject, Unit, ClassDate
+from prework.models import Video
 
 
 class StudentInline(admin.StackedInline):
@@ -15,8 +16,18 @@ class UserAdmin(BaseUserAdmin):
     inlines = (StudentInline,)
 
 
+class VideoInline(admin.StackedInline):
+    model = Video
+    ordering = ('video_num',)
+    extra = 1
+
+
+class ClassDateAdmin(admin.ModelAdmin):
+    inlines = (VideoInline,)
+
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.register(Subject)
 admin.site.register(Unit)
-admin.site.register(ClassDate)
+admin.site.register(ClassDate, ClassDateAdmin)
